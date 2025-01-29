410af4f81336706a4ae86366f62d12689fe428da
Rafael Ribeiro <ribeirorafael2802@gmail.com>
2025-01-29 00:31:54 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,28 @@ const btnAvancar = document.getElementById('btn-avancar');
 const cartoes = document.querySelectorAll('.cartao');
 let cartaoAtual = 0;
 
+cartoes.forEach(cartao => {
+    cartao.addEventListener('click', function () {
+        const cartaVirada = cartao.querySelector('.carta-virada');
+
+        cartao.classList.toggle('virar');
+        cartaVirada.classList.toggle('mostrar-fundo-carta');
+
+        const descricao = cartao.querySelector('descricao');
+        descricao.classList.toggle('esconder');
+        
+    })
+})
+
 btnAvancar.addEventListener('click', function () {
 
-if(cartaoAtual === cartoes.length - 1) return;
-    
-    
+    if (cartaoAtual === cartoes.length - 1) return;
+
+
     const cartaoSelecionado = document.querySelector('.selecionado');
 
     cartaoSelecionado.classList.remove('selecionado');
-    
+
     cartaoAtual++;
     cartoes[cartaoAtual].classList.add('selecionado')
 })
@@ -18,13 +31,13 @@ if(cartaoAtual === cartoes.length - 1) return;
 const btnVoltar = document.getElementById('btn-voltar');
 
 btnVoltar.addEventListener('click', function () {
-    
-    if(cartaoAtual === 0) return;
-        
-        const cartaoSelecionado = document.querySelector('.selecionado');
-        
-        cartaoSelecionado.classList.remove('selecionado');
-        
-        cartaoAtual--;
-        cartoes[cartaoAtual].classList.add('selecionado')
-    })
+
+    if (cartaoAtual === 0) return;
+
+    const cartaoSelecionado = document.querySelector('.selecionado');
+
+    cartaoSelecionado.classList.remove('selecionado');
+
+    cartaoAtual--;
+    cartoes[cartaoAtual].classList.add('selecionado')
+})
